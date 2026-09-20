e9dcba449b2270d1e5ce3194a349500dd604b307
thirashac0730 <thirashac@gmail.com>
2026-09-20 19:53:29 +0530

    Add files via upload

---
+++ code/src/server.js
@@ -0,0 +1,22 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8','8.8.4.4']);
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/db');
+
+// Establish database connection
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+// Start server
+const server = app.listen(PORT, () => {
+  console.log(`Server running in development mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections gracefully
+process.on('unhandledRejection', (err, promise) => {
+  console.error(`Unhandled Rejection Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
